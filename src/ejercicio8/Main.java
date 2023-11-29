92768e45899f813cfecec4468b572b100ff0e135
SamuelDN12 <sdomnic@g.educaand.es>
2023-11-29 14:27:51 +0100

    'Cambios'

--- src/ejercicio8/Main.java
+++ src/ejercicio8/Main.java
@@ -6,6 +6,8 @@
 package ejercicio8;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 class Producto {
@@ -13,12 +15,91 @@ class Producto {
 }
 
 class Recurso {
-    private ArrayList<Integer>
+    private ArrayList<Integer> lista = new ArrayList<>();
+    private int contador = 0;
+    
+    synchronized public void add() {
+        while (lista.size() > 6) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Recurso.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        System.out.println("Depositando el producto " + contador);
+        lista.add(contador);
+        contador++;
+        notifyAll();
+    }
+    
+     synchronized public void get() {
+        while (lista.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Recurso.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        System.out.println("Recogiendo el producto " + lista.get(0));
+        lista.remove(0);
+        notifyAll();
+    }
+    
+}
+
+class Productor extends Thread {
+    
+    Recurso recu; 
+
+    public Productor(Recurso recu) {
+        this.recu = recu;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            recu.add();
+        }
+    }
+    
+}
+
+class Consumidor extends Thread {
+    
+    Recurso recu; 
+
+    public Consumidor(Recurso recu) {
+        this.recu = recu;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            recu.get();
+        }
+    }
+    
 }
 
 public class Main {
 
     public static void main(String[] args) {
+        Recurso recu= new Recurso();
+        Productor productor = new Productor(recu);
+        Consumidor consumidor = new Consumidor(recu);
+        
+        productor.start();
+        consumidor.start();
+        
+        try {
+            productor.join();
+            consumidor.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         
     }
 
